17592f68b522a5ab12234731fd3042d055a09d69
Jukka Karvonen <jukka.karvonen@iki.fi>
2020-02-19 21:29:04 +0200

    remove unused import

--- sal_kiti/settings/local_settings_example.py
+++ sal_kiti/settings/local_settings_example.py
@@ -5,7 +5,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
 import sys
 
 # SECURITY WARNING: keep the secret key used in production secret!
