4cc35c4fe95709c766763539ff3ecb1fed0a0328
Jukka Karvonen <jukka.karvonen@iki.fi>
2020-07-04 15:11:51 +0300

    Management command improvements
    - Fix typo in approve
    - Add command to reset athlete birth date to year only
    
    Signed-off-by: Jukka Karvonen <jukka.karvonen@iki.fi>

---
+++ results/management/commands/athletebirthyearonly.py
@@ -0,0 +1,20 @@
+"""
+Changes all birth dates to year only
+usage: ./manage.py athletebirthyearonly
+"""
+
+from django.core.management.base import BaseCommand
+from results.models.athletes import Athlete
+
+
+class Command(BaseCommand):
+    """Approve records"""
+    args = 'None'
+    help = 'Approve records'
+
+    def handle(self, *args, **options):
+        athletes = Athlete.objects.filter(date_of_birth__isnull=False)
+        for athlete in athletes:
+            date_of_birth = athlete.date_of_birth.replace(month=1, day=1)
+            athlete.date_of_birth = date_of_birth
+            athlete.save()
