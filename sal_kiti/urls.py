83549f16a2d105ff25cde7eac93e04c0c5e44ba2
Jukka Karvonen <jukka.karvonen@iki.fi>
2025-03-08 09:43:25 +0200

    Remove DRF docs and coreapi
    API docs are generated by drf-spectacular.
    
    Signed-off-by: Jukka Karvonen <jukka.karvonen@iki.fi>

--- sal_kiti/urls.py
+++ sal_kiti/urls.py
@@ -3,11 +3,7 @@ from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-)
-from rest_framework.documentation import include_docs_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from results.routers import router
 from results.views.statistics import statistics_pohjolan_malja
@@ -23,7 +19,6 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("admin/", admin.site.urls),
     path("auth/", include("django.contrib.auth.urls")),
-    path("docs/", include_docs_urls(title="SAL Kiti", description="SAL Kiti")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("swagger/", RedirectView.as_view(url="/api/schema/swagger/")),
